9094301cb48cd2a2861b121198d836d81f89668b
lLuckyFzi <levryn04@gmail.com>
2025-04-16 11:42:33 +0700

    study case

---
+++ 01-IF2B-Lucky_Fauzi-M6-Study_Case/trackerExpense.Designer.vb
@@ -0,0 +1,309 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class trackerExpense
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        inputTotalMoney = New TextBox()
+        Label2 = New Label()
+        Panel1 = New Panel()
+        Panel2 = New Panel()
+        inputDetail = New TextBox()
+        Label3 = New Label()
+        inputDate = New DateTimePicker()
+        Label4 = New Label()
+        inputIncome = New RadioButton()
+        inputExpense = New RadioButton()
+        Label5 = New Label()
+        btnAdd = New Button()
+        Label6 = New Label()
+        listData = New ListView()
+        Button1 = New Button()
+        Label7 = New Label()
+        lblBalance = New Label()
+        Panel1.SuspendLayout()
+        Panel2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Raleway", 14.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(318, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(160, 26)
+        Label1.TabIndex = 0
+        Label1.Text = "Tracker Expense"
+        ' 
+        ' inputTotalMoney
+        ' 
+        inputTotalMoney.BackColor = SystemColors.Menu
+        inputTotalMoney.BorderStyle = BorderStyle.None
+        inputTotalMoney.Location = New Point(5, 7)
+        inputTotalMoney.Margin = New Padding(10, 3, 3, 3)
+        inputTotalMoney.Name = "inputTotalMoney"
+        inputTotalMoney.PlaceholderText = "Masukan Jumlah Uang"
+        inputTotalMoney.Size = New Size(190, 16)
+        inputTotalMoney.TabIndex = 1
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Transparent
+        Label2.FlatStyle = FlatStyle.Flat
+        Label2.Font = New Font("Poppins", 9.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(26, 53)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(55, 22)
+        Label2.TabIndex = 2
+        Label2.Text = "Jumlah"
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = SystemColors.Menu
+        Panel1.Controls.Add(inputTotalMoney)
+        Panel1.Location = New Point(26, 78)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(200, 30)
+        Panel1.TabIndex = 3
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = SystemColors.Menu
+        Panel2.Controls.Add(inputDetail)
+        Panel2.Location = New Point(26, 152)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(200, 30)
+        Panel2.TabIndex = 5
+        ' 
+        ' inputDetail
+        ' 
+        inputDetail.BackColor = SystemColors.Menu
+        inputDetail.BorderStyle = BorderStyle.None
+        inputDetail.Location = New Point(5, 7)
+        inputDetail.Margin = New Padding(10, 3, 3, 3)
+        inputDetail.Name = "inputDetail"
+        inputDetail.PlaceholderText = "Masukan keterangan"
+        inputDetail.Size = New Size(190, 16)
+        inputDetail.TabIndex = 1
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.Transparent
+        Label3.FlatStyle = FlatStyle.Flat
+        Label3.Font = New Font("Poppins", 9.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(26, 127)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(79, 22)
+        Label3.TabIndex = 4
+        Label3.Text = "Keterangan"
+        ' 
+        ' inputDate
+        ' 
+        inputDate.CalendarMonthBackground = SystemColors.Menu
+        inputDate.CalendarTitleBackColor = SystemColors.ControlText
+        inputDate.CalendarTitleForeColor = Color.Black
+        inputDate.CalendarTrailingForeColor = SystemColors.ButtonHighlight
+        inputDate.Location = New Point(26, 232)
+        inputDate.Name = "inputDate"
+        inputDate.Size = New Size(200, 23)
+        inputDate.TabIndex = 6
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.BackColor = Color.Transparent
+        Label4.FlatStyle = FlatStyle.Flat
+        Label4.Font = New Font("Poppins", 9.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(26, 207)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(59, 22)
+        Label4.TabIndex = 7
+        Label4.Text = "Tanggal"
+        ' 
+        ' inputIncome
+        ' 
+        inputIncome.AutoSize = True
+        inputIncome.Location = New Point(31, 320)
+        inputIncome.Name = "inputIncome"
+        inputIncome.Size = New Size(86, 19)
+        inputIncome.TabIndex = 8
+        inputIncome.TabStop = True
+        inputIncome.Text = "Pemasukan"
+        inputIncome.UseVisualStyleBackColor = True
+        ' 
+        ' inputExpense
+        ' 
+        inputExpense.AutoSize = True
+        inputExpense.Location = New Point(135, 320)
+        inputExpense.Name = "inputExpense"
+        inputExpense.Size = New Size(91, 19)
+        inputExpense.TabIndex = 9
+        inputExpense.TabStop = True
+        inputExpense.Text = "Pengeluaran"
+        inputExpense.UseVisualStyleBackColor = True
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.BackColor = Color.Transparent
+        Label5.FlatStyle = FlatStyle.Flat
+        Label5.Font = New Font("Poppins", 9.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label5.Location = New Point(26, 284)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(34, 22)
+        Label5.TabIndex = 10
+        Label5.Text = "Tipe"
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.AutoSize = True
+        btnAdd.BackColor = Color.YellowGreen
+        btnAdd.FlatAppearance.BorderSize = 0
+        btnAdd.FlatStyle = FlatStyle.Flat
+        btnAdd.Font = New Font("Poppins Medium", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnAdd.ForeColor = SystemColors.ButtonHighlight
+        btnAdd.Location = New Point(21, 382)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(200, 33)
+        btnAdd.TabIndex = 11
+        btnAdd.Text = "Tambah Data"
+        btnAdd.UseVisualStyleBackColor = False
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.BackColor = Color.Transparent
+        Label6.FlatStyle = FlatStyle.Flat
+        Label6.Font = New Font("Poppins", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label6.Location = New Point(270, 53)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(74, 34)
+        Label6.TabIndex = 12
+        Label6.Text = "Daftar"
+        ' 
+        ' listData
+        ' 
+        listData.CheckBoxes = True
+        listData.FullRowSelect = True
+        listData.GridLines = True
+        listData.Location = New Point(270, 103)
+        listData.Name = "listData"
+        listData.Size = New Size(496, 261)
+        listData.TabIndex = 13
+        listData.UseCompatibleStateImageBehavior = False
+        listData.View = View.Details
+        ' 
+        ' Button1
+        ' 
+        Button1.AutoSize = True
+        Button1.BackColor = Color.FromArgb(CByte(192), CByte(0), CByte(0))
+        Button1.FlatAppearance.BorderSize = 0
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.Font = New Font("Poppins", 9.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button1.ForeColor = SystemColors.ControlLightLight
+        Button1.Location = New Point(647, 53)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(119, 33)
+        Button1.TabIndex = 14
+        Button1.Text = "Delete"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.BackColor = Color.Transparent
+        Label7.FlatStyle = FlatStyle.Flat
+        Label7.Font = New Font("Poppins", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label7.Location = New Point(270, 380)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(78, 34)
+        Label7.TabIndex = 15
+        Label7.Text = "Saldo: "
+        ' 
+        ' lblBalance
+        ' 
+        lblBalance.AutoSize = True
+        lblBalance.BackColor = Color.Transparent
+        lblBalance.FlatStyle = FlatStyle.Flat
+        lblBalance.Font = New Font("Poppins", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lblBalance.Location = New Point(341, 380)
+        lblBalance.Name = "lblBalance"
+        lblBalance.Size = New Size(44, 34)
+        lblBalance.TabIndex = 16
+        lblBalance.Text = "Rp."
+        ' 
+        ' trackerExpense
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.White
+        ClientSize = New Size(800, 439)
+        Controls.Add(lblBalance)
+        Controls.Add(Label7)
+        Controls.Add(Button1)
+        Controls.Add(listData)
+        Controls.Add(Label6)
+        Controls.Add(btnAdd)
+        Controls.Add(Label5)
+        Controls.Add(inputExpense)
+        Controls.Add(inputIncome)
+        Controls.Add(Label4)
+        Controls.Add(inputDate)
+        Controls.Add(Panel2)
+        Controls.Add(Panel1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        FormBorderStyle = FormBorderStyle.FixedToolWindow
+        Name = "trackerExpense"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Tracker Expense"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents inputTotalMoney As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents inputDetail As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents inputDate As DateTimePicker
+    Friend WithEvents Label4 As Label
+    Friend WithEvents inputIncome As RadioButton
+    Friend WithEvents inputExpense As RadioButton
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents listData As ListView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents lblBalance As Label
+
+End Class
---
+++ 01-IF2B-Lucky_Fauzi-M6-Study_Case/trackerExpense.vb
@@ -0,0 +1,81 @@
+﻿Public Class trackerExpense
+    Dim transactionList As New List(Of ModelTransaction)
+
+    Private Sub trackerExpense_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        With listData
+            .Columns.Add("Date", 100, HorizontalAlignment.Left)
+            .Columns.Add("Type", 100, HorizontalAlignment.Left)
+            .Columns.Add("Total", 100, HorizontalAlignment.Left)
+            .Columns.Add("Details", 200, HorizontalAlignment.Left)
+            .View = View.Details
+            .CheckBoxes = True
+            .FullRowSelect = True
+        End With
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim dateValue As String = inputDate.Value.ToShortDateString()
+        Dim total As Integer = CInt(inputTotalMoney.Text)
+        Dim type As String = If(inputIncome.Checked, "Pemasukan", "Pengeluaran")
+        Dim detail As String = inputDetail.Text
+
+        transactionList.Add(New ModelTransaction(dateValue, total, type, detail))
+
+        inputDate.Value = DateTime.Now
+        inputTotalMoney.Clear()
+        inputDetail.Clear()
+        inputIncome.Checked = False
+        inputExpense.Checked = False
+
+        showTransactions()
+        balanceCount()
+        ResetForm()
+    End Sub
+
+    Public Sub showTransactions()
+        listData.Items.Clear()
+        For Each tr In transactionList
+            Dim item As New ListViewItem(tr.TransactionDate.ToShortDateString)
+            item.SubItems.Add(tr.Type)
+            item.SubItems.Add(tr.Total)
+            item.SubItems.Add(tr.Description)
+            listData.Items.Add(item)
+        Next
+    End Sub
+
+    Public Sub balanceCount()
+
+        Dim balance As Integer = 0
+
+        For Each tr In transactionList
+            If tr.Type = "Pemasukan" Then
+                balance += tr.Total
+            Else
+                balance -= tr.Total
+            End If
+        Next
+        lblBalance.Text = balance.ToString("N0")
+    End Sub
+
+    Public Sub ResetForm()
+        inputTotalMoney.Clear()
+        inputDetail.Clear()
+        inputIncome.Checked = False
+        inputExpense.Checked = False
+    End Sub
+End Class
+
+Public Class ModelTransaction
+    Public Property TransactionDate As Date
+    Public Property Total As Integer
+    Public Property Type As String
+    Public Property Description As String
+
+    Public Sub New(tDate As Date, total As Integer, type As String, desc As String)
+        Me.TransactionDate = tDate
+        Me.Total = total
+        Me.Type = type
+        Me.Description = desc
+    End Sub
+
+End Class
